a745aac370683ade70a71610879b8a19f0560e82
calebparks <caleb.parks@ispot.tv>
2025-05-13 13:14:32 -0700

    move code to seperate file

---
+++ bitbucket.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/ktrysmt/go-bitbucket"
+)
+
+func getRepo(bb *bitbucket.Client, owner string, repoName string) *bitbucket.Repository {
+	ro := &bitbucket.RepositoryOptions{
+		Owner:    owner,
+		RepoSlug: repoName,
+	}
+	repo, err := bb.Repositories.Repository.Get(ro)
+	if err != nil {
+		panic(err)
+	}
+	return repo
+}
+
+// clones repo to a temp folder
+func cloneRepo(owner string, repo string) (tempfolderpath string) {
+	tempDir, err := os.MkdirTemp("", fmt.Sprintf("%s-%s-*", owner, repo))
+	if err != nil {
+		log.Fatalf("Failed to create temp directory: %s", err)
+	}
+
+	cloneURL := fmt.Sprintf("https://bitbucket.org/%s/%s.git", owner, repo)
+	fmt.Printf("Cloning repository %s to %s\n", repo, tempDir)
+
+	cmd := exec.Command("git", "clone", cloneURL, tempDir)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Fatalf("Failed to clone repository: %s\nOutput: %s", err, string(output))
+	}
+	fmt.Println(string(output))
+
+	return tempDir
+}
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"os/exec"
 
 	"github.com/google/go-github/v72/github"
 	"github.com/joho/godotenv"
@@ -66,36 +65,4 @@ func migrateRepo(gh *github.Client, bb *bitbucket.Client, bbOrg string, ghOrg st
 	//prs := getPrs(bb, owner, repoName, repo.Mainbranch.Name)
 	//fmt.Println("mock migrating prs: ", prs.Values[0].Title)
 	fmt.Println("done migrating repo")
-}
-
-func getRepo(bb *bitbucket.Client, owner string, repoName string) *bitbucket.Repository {
-	ro := &bitbucket.RepositoryOptions{
-		Owner:    owner,
-		RepoSlug: repoName,
-	}
-	repo, err := bb.Repositories.Repository.Get(ro)
-	if err != nil {
-		panic(err)
-	}
-	return repo
-}
-
-// clones repo to a temp folder
-func cloneRepo(owner string, repo string) (tempfolderpath string) {
-	tempDir, err := os.MkdirTemp("", fmt.Sprintf("%s-%s-*", owner, repo))
-	if err != nil {
-		log.Fatalf("Failed to create temp directory: %s", err)
-	}
-
-	cloneURL := fmt.Sprintf("https://bitbucket.org/%s/%s.git", owner, repo)
-	fmt.Printf("Cloning repository %s to %s\n", repo, tempDir)
-
-	cmd := exec.Command("git", "clone", cloneURL, tempDir)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatalf("Failed to clone repository: %s\nOutput: %s", err, string(output))
-	}
-	fmt.Println(string(output))
-
-	return tempDir
 }
